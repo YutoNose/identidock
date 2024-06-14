4b152c04ee4e906103af59ce3ab2e6ed4f8442f6
YutoNose <110585253+YutoNose@users.noreply.github.com>
2024-06-14 19:49:08 +0900

    Update identidock.py

--- app/identidock.py
+++ app/identidock.py
@@ -40,7 +40,7 @@ def get_monster(name):
     return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=9090)
 
 
 
