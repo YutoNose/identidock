1a990920bb1d73bd75ed23d60848cb383457d71a
YutoNose <110585253+YutoNose@users.noreply.github.com>
2024-06-15 11:53:47 +0900

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
+# Set the working directory to the correct path
+WORK_DIR="/var/jenkins_home/workspace/identidock/app"
+
 if [ "$ENV" = "DEV" ]; then
     echo "Running Development Server"
-    exec python "/app/identidock.py"
+    exec python "$WORK_DIR/identidock.py"
 elif [ "$ENV" = "UNIT" ]; then
     echo "Running Unit test"
-    exec python "/app/tests.py"
+    exec python "$WORK_DIR/tests.py"
 else
     echo "Running Production Server"
-    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file "$WORK_DIR/identidock.py" --callable app --stats 0.0.0.0:9191
 fi
- 
